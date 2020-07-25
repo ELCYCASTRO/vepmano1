912f5bfd72b1a1e9b682f011627ba72f45baf77a
ELCYCASTRO <elcycastrojunior@gmail.com>
2020-07-24 21:44:48 -0400

    projeto pronto

---
+++ test/Teste.java
@@ -0,0 +1,28 @@
+
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author elcy.arruda
+ */
+public class Teste {
+    public static void main(String[] args) {
+        JFrame janela = new JFrame();
+        janela.setSize(300,400);
+        janela.setTitle("CÃO");
+        janela.setVisible(true);
+        
+        
+        
+        
+        
+        
+    }
+    
+}
